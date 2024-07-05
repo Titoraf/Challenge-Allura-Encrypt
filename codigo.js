844e79d919e7dacfca0f5d48b8e1a47f7d0897b0
Titoraf <titoraf@hotmail.com>
2024-07-05 15:46:14 -0400

    Commit programa encriptación

---
+++ codigo.js
@@ -0,0 +1,97 @@
+let textoSinEncriptar = '';
+let textoEncriptado = '';
+let valorPlaceholder = '';
+const copiarContenido = '';
+
+function containsUppercase(str) {
+    return /[A-Z]/.test(str);
+  }
+
+
+function encriptarTexto() {
+
+
+    textoSinEncriptar = document.getElementById("textoAEncriptar").value;
+    // valorPlaceholder = document.getElementById("textoAEncriptar").placeholder;
+    
+
+    if (textoSinEncriptar.length == '' ) {
+        
+        // console.log("SE EJECUTA LA VALIDACIÓN")
+        
+        document.getElementById("textoAEncriptar").placeholder = '¡No indicaste el texto, por favor colócalo!'
+    //    textoSinEncriptar = ''
+        return;
+    }
+
+    else {
+
+        if (containsUppercase(textoSinEncriptar) == true){
+            document.getElementById("textoAEncriptar").value = ''
+            document.getElementById("textoAEncriptar").placeholder = '¡Recuerda no poner mayúsculas!'
+
+            return;
+        }
+
+        else {
+
+    document.getElementById("textoAEncriptar").placeholder = 'Ingrese el texto aquí'
+
+    textoSinEncriptar = textoSinEncriptar.replaceAll("e","enter")
+    textoSinEncriptar = textoSinEncriptar.replaceAll("i","imes")
+    textoSinEncriptar = textoSinEncriptar.replaceAll("a","ai")
+    textoSinEncriptar = textoSinEncriptar.replaceAll("o","ober")
+    textoSinEncriptar = textoSinEncriptar.replaceAll("u","ufat")
+
+    textoEncriptado = textoSinEncriptar;
+
+ document.getElementById("contenidoEncriptado").innerHTML = textoEncriptado;
+
+    // document.getElementById("textoAEncriptar").value = ''
+//Oculta la imagen y letras del cuadro y muestra el texto encriptado
+    document.getElementById("contenidoCaja").style.display = "none"
+    document.getElementById("verContenidoEncriptado").style.display ="block"
+}
+}
+
+}
+
+function desencriptarTexto() {
+
+    if (textoSinEncriptar.length == '' ) {
+        
+        // console.log("SE EJECUTA LA VALIDACIÓN")
+        
+        document.getElementById("textoAEncriptar").placeholder = '¡No indicaste el texto, por favor colócalo!'
+    //    textoSinEncriptar = ''
+        return;
+    }
+
+    else {
+
+    textoSinEncriptar = textoSinEncriptar.replaceAll("enter","e")
+    textoSinEncriptar = textoSinEncriptar.replaceAll("imes","i")
+    textoSinEncriptar = textoSinEncriptar.replaceAll("ai","a")
+    textoSinEncriptar = textoSinEncriptar.replaceAll("ober","o")
+    textoSinEncriptar = textoSinEncriptar.replaceAll("ufat","u")
+
+    document.getElementById("contenidoEncriptado").innerHTML = textoSinEncriptar;
+}
+}
+
+function copiarTextoEncriptado() {
+    
+    console.log("Se ve el evento onclick")
+
+    navigator.clipboard.writeText(textoSinEncriptar)
+    .then(() => {
+        console.log('Contenido copiado al portapapeles');
+        /* Resuelto - texto copiado al portapapeles con éxito */
+      },() => {
+        console.error('Error al copiar');
+        /* Rechazado - fallo al copiar el texto al portapapeles */
+      });
+
+      
+
+}
